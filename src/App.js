4bfef64e45523b9326555760be465f6464fdddee
meryem <meryemaydmr41@gmail.com>
2024-04-17 15:05:53 +0300

    some fixes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import AppRouter from "../src/router/AppRouter.js"
 import "./App.css";
+
 
 function App() {
  
   return (
     <div className="App">
-     
+     <AppRouter/>
     </div>
   );
 }
